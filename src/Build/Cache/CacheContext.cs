700f94cee879212dc48dcfae3020b4196b1dce8e
Mihai Codoban <micodoba@microsoft.com>
2020-07-01 18:53:36 -0700

    Add ProjectCache plugin

---
+++ src/Build/Cache/CacheContext.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Graph;
+
+namespace Microsoft.Build.Cache
+{
+    public class CacheContext
+    {
+        public ProjectGraph Graph { get; }
+    }
+}
